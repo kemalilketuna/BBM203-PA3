ab793d9efebb00efa0dc79d397f8f389d83f47c3
Kemal İlke Tuna <35730029+kemalilketuna@users.noreply.github.com>
2023-12-01 00:30:43 +0300

    init

---
+++ Network.cpp
@@ -0,0 +1,34 @@
+#include "Network.h"
+
+Network::Network() {
+
+}
+
+void Network::process_commands(vector<Client> &clients, vector<string> &commands, int message_limit,
+                      const string &sender_port, const string &receiver_port) {
+    // TODO: Execute the commands given as a vector of strings while utilizing the remaining arguments.
+    /* Don't use any static variables, assume this method will be called over and over during testing.
+     Don't forget to update the necessary member variables after processing each command. For example,
+     after the MESSAGE command, the outgoing queue of the sender must have the expected frames ready to send. */
+}
+
+vector<Client> Network::read_clients(const string &filename) {
+    vector<Client> clients;
+    // TODO: Read clients from the given input file and return a vector of Client instances.
+    return clients;
+}
+
+void Network::read_routing_tables(vector<Client> &clients, const string &filename) {
+    // TODO: Read the routing tables from the given input file and populate the clients' routing_table member variable.
+}
+
+// Returns a list of token lists for each command
+vector<string> Network::read_commands(const string &filename) {
+    vector<string> commands;
+    // TODO: Read commands from the given input file and return them as a vector of strings.
+    return commands;
+}
+
+Network::~Network() {
+    // TODO: Free any dynamically allocated memory if necessary.
+}
